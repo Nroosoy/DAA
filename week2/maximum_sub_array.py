53f7ff52b32ffd95065868a65611fc89d5cdd22d
Nico Roos <nico@nroos.fi>
2021-09-19 20:30:33 +0300

    week2 max sub

---
+++ week2/maximum_sub_array.py
@@ -0,0 +1,21 @@
+def max_sub(A):
+    max = -99999 #just something smaller than any value in the arrays
+    max_start = 0
+    max_end = 0
+    curr_start = 0
+    max_here = 0
+    for (i, v) in enumerate(A):
+        print(max_here, i, max)
+        max_here += v
+        if max < max_here:
+            max = max_here
+            max_end = i
+            max_start = curr_start
+        if max_here < 0:
+            max_here = 0
+            # next maximum segment must start at earliest from the next index
+            curr_start = i+1
+    return [max_start, max_end, max]
+
+
+print(max_sub([1, 2, 3, -8, 4, 3, -1, 2]))
