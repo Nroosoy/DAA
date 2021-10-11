ab4ca95972b3f39e718fae48d8b030ca601073a5
Nico Roos <nico@nroos.fi>
2021-10-11 17:27:54 +0000

    Week5 knapsack with map.

--- week5/unbounded_knapsack.py
+++ week5/unbounded_knapsack.py
@@ -1,11 +1,38 @@
-def knapsack(items, w):
-    V = [0 for x in range(w+1)]
-    for (i, _) in enumerate(V):
-        for (j, _) in enumerate(items):
-            if items[j][0] <= i:
-                V[i] = max(V[i], V[i-items[j][0]] + items[j][1])
-        
-    return V
+""""Week 5 exercises regarding knapsack problem"""
+
+import datetime
+
+def knapsack(items, max_weight):
+    """
+    Classical DP solution to knapsack problem
+    items       -- an iterable containing tuples (weight, value)
+    max_weight  -- integer indicating the max weight the knapsack can hold
+    """
+    solutions = [0 for x in range(max_weight+1)]
+    for i in range(0, max_weight + 1):
+        for (weight, value) in items:
+            if weight <= i:
+                solutions[i] = max(solutions[i], solutions[i - weight] + value)
+
+    return solutions
+
+def get_solution(solutions, weight):
+    """"Gets the solution from the given map"""
+    if weight in solutions:
+        return solutions[weight]
+    return 0
+
+def knapsack_map(items, max_weight):
+    """Same as the traditional DP solution but uses a map instead of an array"""
+    solutions = {}
+    for i in range(0, max_weight + 1):
+        for (weight, value) in items:
+            if weight <= i:
+                solutions[i] = max(
+                    get_solution(solutions, weight),
+                    get_solution(solutions, i - weight) + value
+                )
+    return solutions
 
 # (weight, value)
 items_arr = ((1, 10), (3, 40), (4, 50), (5, 70))
